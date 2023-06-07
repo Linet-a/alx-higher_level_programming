b5252c3244ea9975a58decc84fd1989cbc08b537
Linet Okinyi <linetawino2018@gmail.com>
2023-06-07 17:55:35 +0300

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(10):
     for j in range(i+1, 10):
         print("{:02d}".format(i), end=", ")
-        print("{:02d}".format(j), end= ", " i j < 9 else "\n")
+        print("{:02d}".format(j), end=", " if j < 9 else "\n")
