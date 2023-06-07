9032e4cdc70d95ce508959f336e46d660b107851
Linet Okinyi <linetawino2018@gmail.com>
2023-06-07 22:48:12 +0300

    -uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    res_str = ''
+    for ch in str:
+        if 'a' <= ch <= 'z':
+            res_str += chr(ord(ch) - 32)
+        else:
+            res_str += ch
+    print(res_str + '\n')
