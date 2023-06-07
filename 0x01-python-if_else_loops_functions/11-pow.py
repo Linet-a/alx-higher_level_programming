08af0b0b5f49bff3c6547e23f8f0d6f8d38eceee
Linet Okinyi <linetawino2018@gmail.com>
2023-06-08 00:12:18 +0300

    update pow function

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
+    if b < 0:
+        result = 1 / pow(a, -b)
+    elif b == 0:
+        result = 1
+    else:
+        result = 1
+        for _ in range(b):
+            result *= a
+    if a < 0 and b % 2 != 0:
+        result = -result
     return result
