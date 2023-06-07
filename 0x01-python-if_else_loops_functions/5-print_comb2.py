ff30beee0880531ae78035f73ee72958aec341d3
Linet Okinyi <linetawino2018@gmail.com>
2023-06-07 17:12:18 +0300

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for number in range(100):
-    if(number != 99):
-        print("{:02d}".format(number) + ", ", end='')
-    else:
-        print("{:02d}".format(number))
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
