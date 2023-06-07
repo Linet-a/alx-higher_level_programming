fb805ad1fc02fd0779e4f1d80d465e0ca8870b4c
Linet Okinyi <linetawino2018@gmail.com>
2023-06-07 23:00:17 +0300

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     res_str = ''
     for ch in str:
-        if 97 <= ord(ch) <= 122:
+        if ch >= 97 and ch <= 122:
             res_str += chr(ord(ch) - 32)
         else:
             res_str += ch
