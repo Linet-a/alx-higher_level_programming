b094a1232cdd6c4a2ee47017870de55686d581f5
Linet Okinyi <linetawino2018@gmail.com>
2023-06-08 22:53:41 +0300

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_vector = len(sys.argv)
+    if (arg_vector <= 1):
+        print("{} arguments:".format(arg-vector -1)
+    elif (arg_vector == 2):
+        print("{}argiments:".format(arg-vector -1)
+    else:
+        ("{} arguments:".format(arg_vector -1))
+    for arg_c in range(1, arg_vector):
+        print("{}: {}".format(arg_c, sys.argv[arg_c]))
