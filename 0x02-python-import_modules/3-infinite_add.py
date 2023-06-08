f2ae27c189a3be41bfefbe09b93bbca8c7521c6d
Linet Okinyi <linetawino2018@gmail.com>
2023-06-08 23:19:30 +0300

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for n in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ == "__main__":
+    import sys
+    grand_total = 0
+    for num in range(len(sys.argv) - 1):
+        grand_total += int(sys.argv[num + 1])
+    print("{}".format(grand_total))
