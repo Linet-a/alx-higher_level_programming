3d66d6836ac762c1068c598397622665ea72e21d
Linet Okinyi <linetawino2018@gmail.com>
2023-06-10 22:59:35 +0300

    prints matrix of intergers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+vim def print_matrix_integer(matrix=[[]]):
+    for elements in matrix:
+        for i in element:
+            print('{:d}'.format(i), end=' ')
+        print()
