183aadf8f0f3ab8d44babe195d913aa7973e4fb4
Linet Okinyi <linetawino2018@gmail.com>
2023-06-10 22:32:27 +0300

    removes C and c from a str

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    for ch in my_string:
-        if ch == 'c' or ch == 'C':
-            my_string.replace(ch, '')
-        return my_string
+    return my_string.replace('c', '').replace('C', '')
