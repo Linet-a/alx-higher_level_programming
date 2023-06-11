1387349b4a49d4d2ff514644f49414b89870c6c2
Linet Okinyi <linetawino2018@gmail.com>
2023-06-11 20:21:59 +0300

    adds two tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a = len(tuple_a)
-    len_b = len(tupple_b)
+    tuple_a = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
+    tuple_b = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
 
-    if len_a < 1:
-        tuple_a = (0, 0)
-    elif len_a < 2:
-        tuple_a = (tuple_a[0], 0)
-    if len_b < 1:
-        tupple_b = (0, 0)
-    elif len_b < 2:
-        tuple_b = (tuple_b[0], 0)
-    tuple_comb = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return tuple_comb
+    sum_first = tuple_a[0] + tuple_b[0]
+    sum_second = tuple_a[1] + tuple_b[1]
+
+    return (sum_first, sum_second)
