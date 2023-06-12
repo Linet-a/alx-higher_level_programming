b33c1da9aa8d8f34d6b8e08db0dea17645ea6038
Linet Okinyi <linetawino2018@gmail.com>
2023-06-12 20:05:39 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     arg_str = "argument" if num_args == 1 else "arguments"
     separator = ":" if num_args > 0 else "."
     
-    print("Number of {}{}{}".format(num_args, " " + arg_str if num_args > 0 else "", separator))
+    print("{}{}{}".format(num_args, " " + arg_str if num_args > 0 else "", separator))
     
     for i in range(num_args):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
