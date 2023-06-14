1df3a956d26ee9017e586e8ca5dec14e4d7eec6e
Linet Okinyi <linetawino2018@gmail.com>
2023-06-14 21:04:31 +0300

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for k in dictionay.keys():
+        if k != key:
+            value = a_dictionary[key]
+    return a_dictionary
