83f487cbf8e96ba16243661788782c6e29c44d49
Linet Okinyi <linetawino20182gmail.com>
2023-06-26 23:44:42 +0300

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        print() #print new line after the integer
-        return True
     except (ValueError, TypeError):
         return False
-
+    else:
+        return True
