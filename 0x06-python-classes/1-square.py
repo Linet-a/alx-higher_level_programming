0e64fe7ca1c4d85c5e3d9c5b843a3c0df81bf88a
Linet Okinyi <linetawino20182gmail.com>
2023-06-27 23:41:03 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a class Square that defines a square by its size
+"""
+
+
+def __init__(self, size):
+    """
+    Initializes a Square instance with a given size.
+
+
+    Args:
+        size(int): The size of the square
+    Raises:
+        None
+    """
+    self.__size = size
