f9b312b49a401e443f7c78f256b86155141af4bb
Linet Okinyi <linetawino20182gmail.com>
2023-07-04 18:52:29 +0300

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python3
 """
-Rectangle class definition
+Defines a class Rectangle
 """
 
 
 class Rectangle:
-    """class representation of a rectangle"""
+    """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
-        """initializes the rectangle"""
-        self._width = width
-        self._height = height
+        """Initializes the rectangle"""
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
         """getter for the private instance attribute width"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """setter for the private instance attribute width"""
-        if isinstance(value, int):
-            if value >= 0:
-                self._width = value
-            else:
-                raise ValueError("width must be >= 0")
-        else:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
-        """getter for the private attribute height"""
-        return self._height
+        """getter for the private instance attribute height"""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        if isinstance(value, int):
-            if value >= 0:
-                self._height = value
-            else:
-                raise ValueError("height must be >= 0")
-        else:
+        """setter for the private instance attribute height"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
