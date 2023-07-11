c3bdeaeb7e417cd554e980100bfabbf8ebd9c40c
Linet Okinyi <linetawino20182gmail.com>
2023-07-11 17:33:21 +0300

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+contains the read_file function
+"""
+
+
 def read_file(filename=""):
     """Reads text file and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
-       print( f.read(), end="")
+       print(f.read(), end="")
