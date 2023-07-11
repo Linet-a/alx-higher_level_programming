0733b52e37bc236f918f4b55e9daf25de9225d7d
Linet Okinyi <linetawino20182gmail.com>
2023-07-11 18:09:33 +0300

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+contains append_write function
+"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
