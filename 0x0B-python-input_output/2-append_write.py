b5d151c636e037d37d52f2a8a63842ae94ed82cd
Linet Okinyi <linetawino20182gmail.com>
2023-07-11 18:12:21 +0300

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,6 @@ contains append_write function
 
 
 def append_write(filename="", text=""):
+    """returns number of characters added"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
