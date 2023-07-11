34cd5c3c8c28a16cd16f09a32ea2b444813095e5
Linet Okinyi <linetawino20182gmail.com>
2023-07-11 17:59:21 +0300

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+contains write file function
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes a string to text file(utf8)
+    returns the characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text))
